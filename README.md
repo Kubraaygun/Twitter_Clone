067fae1d3601a56d4ad063962b85da5c6b052ce3
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-19 22:35:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+react-router-dom
+react-toastify
+react-icons
+moment
+uuid
+firebase
+tailwind
+ 
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgezAUvd2SZNxH5cA-Mqq1euFHJVSg0L8",
+  authDomain: "twitter-8a16c.firebaseapp.com",
+  projectId: "twitter-8a16c",
+  storageBucket: "twitter-8a16c.appspot.com",
+  messagingSenderId: "719303331229",
+  appId: "1:719303331229:web:09103850d9540de2d17374"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

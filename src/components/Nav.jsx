2a4ef18a871743396247253a7752a295f21372a2
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-30 00:25:41 +0300

    Nav duzenlendi

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { navSections } from "./../contants/index";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <div className="flex flex-col justify-between items-end px-2 py-4">
+      {/**Linkler */}
 
-export default Nav
+      <div>
+        <img className="w-14 mb-4" src="x-logo.webp " />
+
+        {navSections.map((i) => (
+          <div className="flex justify-center md:justify-normal items-center gap-3 text-2xl md:text-xl p-3 cursor-pointer transition rounded-lg hover:bg-[#505050b7] ">
+            {i.icon}
+            <span className="max-md:hidden whitespace-nowrap">{i.title}</span>
+          </div>
+        ))}
+      </div>
+
+      {/**Kullanici Bilgileri */}
+    </div>
+  );
+};
+
+export default Nav;

95ced991856d69ee4abb4e91068ec15856b34ccd
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-30 14:11:18 +0300

    auth tamamlandi

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
+import { BiDoorOpen } from "react-icons/bi";
 import { navSections } from "./../contants/index";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/config";
 
-const Nav = () => {
+const Nav = ({ user }) => {
   return (
     <div className="flex flex-col justify-between items-end px-2 py-4">
       {/**Linkler */}
@@ -17,6 +20,24 @@ const Nav = () => {
       </div>
 
       {/**Kullanici Bilgileri */}
+      <div>
+        {!user ? (
+          <div className="w-12 h-12 bg-gray-700 rounded-full animate-bounce"></div>
+        ) : (
+          <div className="flex flex-col gap-5">
+          <div className="flex gap-2 items-center">
+            <img className="w-12 h-12 rounded-full" src={user?.photoURL} />
+            <p className="max-md:hidden">{user.displayName}</p>
+          </div>
+
+<button onClick={()=>signOut(auth)} className="flex justify-center p-1 gap-2 items-center bg-gray-700 rounded text-2xl md:text-[15px] transition hover:bg-gray-800">
+  <BiDoorOpen/>
+  <span className="max-md:hidden">Çıkış Yap</span>
+</button>
+
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
 import Aside from "../components/Aside";
 import Main from "../components/Main";
 import Nav from "../components/Nav";
 import { auth } from "../firebase/config";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
 const FeedPage = () => {
+  const [user, setUser] = useState(null);
+  //Kullanici bilgisine abone ol
+  useEffect(() => {
+    //kullanici oryemunu izle kullanici state'e aktar
+    const unsub = onAuthStateChanged(auth, (currUser) => setUser(currUser));
+    //sayfadan ayrilirsa izlemeyi sonlandir
+    return () => unsub();
+  }, []);
   return (
-<div className="feed h-screen bg-black overflow-hidden">
-  <Nav/>
-  <Main/>
-  <Aside/>
-</div>
+    <div className="feed h-screen bg-black overflow-hidden">
+      <Nav user={user}/>
+      <Main />
+      <Aside />
+    </div>
   );
 };
 

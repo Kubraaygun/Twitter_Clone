6ef63adf44df7dbe7362bc8f9f85c9f73c80d0bb
kubraaygun <kubraaygun@outlook.com.tr>
2024-04-30 16:28:25 +0300

    componentler eklendi

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,40 @@
-import Form  from "./Form";
+import { useEffect, useState } from "react";
+import Form from "./Form";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/config";
+import Spinner from './Spinner';
+import Post from "./Post";
 
-const Main = ({user}) => {
+const Main = ({ user }) => {
+  const [tweets, setTweets] = useState(null);
+  const tweetsCol = collection(db, "tweets");
+  //tweet koleksiyonundaki verileri anlik olarak al
+  useEffect(() => {
+    const unsub = onSnapshot(tweetsCol, (snapshot) => {
+      //gecici dizi
+      const tempTweets = [];
+      //butun dokumanlari don
+      snapshot.forEach((doc) => {
+        tempTweets.push({ id: doc.id, ...doc.data() });
+      });
+      //veriyi State'e aktar
+      setTweets(tempTweets);
+    });
+    //sayfadan cikinca aboneligi bitir
+    return () => unsub();
+  }, []);
   return (
     <main className="border border-gray-700 overflow-y-auto">
       <header className="font-bold p-4 border-b-[1px] border-gray-700">
         Anasayfa
       </header>
       <Form user={user} />
+
+      {!tweets ? (
+       <Spinner style={'w-6 h-6 mx-auto my-10'} />
+      ) : (
+        tweets.map((tweet) => <Post key={tweet.id} tweet={tweet}/>)
+      )}
     </main>
   );
 };

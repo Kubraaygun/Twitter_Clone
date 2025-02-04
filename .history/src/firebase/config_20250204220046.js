170b1a58507b34d91a0a58c9135ac8024f6d8235
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-02-04 22:01:40 +0300

    update config

--- .history/src/firebase/config_20250204220046.js
+++ .history/src/firebase/config_20250204220046.js
@@ -2,31 +2,32 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  authDomain: "twitter-8a16c.firebaseapp.com",
+  projectId: "twitter-8a16c",
+  storageBucket: "twitter-8a16c.appspot.com",
+  messagingSenderId: "719303331229",
+  appId: "1:719303331229:web:09103850d9540de2d17374"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+
 //auth yapisinin referansini alma
-export const auth = getAuth(app);
+export const auth = getAuth(app)
 
 //google saglayacisinin referansini alma
-export const provider = new GoogleAuthProvider();
+export const provider=new GoogleAuthProvider()
 
 //veritabani referansini al
-export const db = getFirestore(app);
+export const db= getFirestore(app)
 
 //storage referansini al
-export const storage = getStorage(app);
+export const storage=getStorage(app) 
